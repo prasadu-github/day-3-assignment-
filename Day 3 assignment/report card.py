ab0396d1d6f9bf4139fbd9ac075e7633502a4d6e
prasadu-github <75842388+prasadu-github@users.noreply.github.com>
2020-12-11 19:37:07 +0530

    Day 3 assignment submitted

---
+++ Day 3 assignment/report card.py
@@ -0,0 +1,18 @@
+marks = int(input("enter your marks in between 100 : "))
+if marks >= 90 :
+    print("yor grade is A+ grade")
+elif marks >= 80:
+    print("yor grade is A grade")
+elif marks >=70:
+    print("yor grade is B+ grade")
+elif  marks >= 60:
+    print("yor grade is B grade")
+elif marks >= 50:
+    print("yor grade is C+ grade")
+elif marks >= 40 :
+    print("yor grade is C grade")
+elif marks >= 35:
+    print("yor grade is D grade")
+else:
+    print("sorry you are failed")
+
